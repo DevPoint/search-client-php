c83543454fb911d0e0a554023454ee9cb6133f68
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-09 16:17:30 +0200

    added functions update and delete

--- src/Contracts/SearchClient.php
+++ src/Contracts/SearchClient.php
@@ -5,6 +5,40 @@ namespace Devpoint\SearchClient\Contracts;
 interface SearchClient {
     
     /**
+     * Update values on the index.
+     * 
+     * [
+     *   'id': {valueId}
+     *   'data': [
+     *     'name': {the_name}
+     *     'email': {the_email}
+     *     'street': {the_street}
+     *      ..
+     *   ]
+     * ]
+     *
+     * Remark:
+     * Updates may happen sync or async.
+     *
+     * @param  string  $index
+     * @param  array   $valuesWithMeta
+     * @return void
+     */
+    public function update($index, $valuesWithMeta);
+
+    /**
+     * Delete values on the index.
+     *
+     * Remark:
+     * Updates may happen sync or async.
+     *
+     * @param  string  $index
+     * @param  array   $ids
+     * @return void
+     */
+    public function delete($index, $ids);
+
+    /**
      * Create new search builder instance
      *
      * @param  string  $index
